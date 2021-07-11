c54c498f9f6fa253562a42d107056ee9d2c27218
aakriti <74606667+aakriti-06@users.noreply.github.com>
2021-07-11 12:26:33 +0530

    Update changecolor.js

--- Change background color/changecolor.js
+++ Change background color/changecolor.js
@@ -1,1 +1,8 @@
-
+const colorBtn=document.querySelector('.colorBtn');
+const bcg=document.querySelector('body');
+const colors = ['blue','yellow','red','green','pink','purple','#3b5998'];
+colorBtn.addEventListener('click',changecolor);
+function changecolor(){
+    let random=Math.floor(Math.random()*colors.length)
+    bcg.style.backgroundColor=colors[random];
+}
