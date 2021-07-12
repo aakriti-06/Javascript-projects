eca212c8db1c2f2e1413ec334977cf7fb701a548
aakriti <74606667+aakriti-06@users.noreply.github.com>
2021-07-12 13:03:38 +0530

    Update quotes.js

--- Change Quotes/quotes.js
+++ Change Quotes/quotes.js
@@ -1,1 +1,31 @@
-
+const quotes = [
+    {
+        name:'Stephan King',
+        quote:'Get busy living or get busy dying.'
+    },
+    {
+        name:'John F.Kennedy',
+        quote:'Those who dare to fail miserably can achieve greatly'
+    },
+    {
+        name:'Abraham Lincoln',
+        quote:"I'm a success today because I had a friend who believed in me and I didn't have the heart to let him down."
+    },
+    {
+        name:'Leonardo Da Vinci',
+        quote:'It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happended to things.'
+    },
+    {
+        name:'Leo Tolstoy',
+        quote:'If you want to be happy,be.'
+    }
+]
+const quoteBtn=document.querySelector('#quoteBtn');
+const quoteAuthor=document.querySelector('#quoteAuthor');
+const quote=document.querySelector('#quote');
+quoteBtn.addEventListener('click',displayQuote);
+function displayQuote(){
+  let random=Math.floor(Math.random()*quotes.length);
+  quoteAuthor.innerHTML=quotes[random].name;
+  quote.innerHTML=quotes[random].quote;
+}
